3d13bb5b6adc358c35b8f9cee1d75258ba09b931
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-03-29 18:03:13 +0800

    Update PaintF.java
    Just go add more buttons and the screen got wider too

--- src/paint/PaintF.java
+++ src/paint/PaintF.java
@@ -13,7 +13,7 @@ public class PaintF extends JFrame
     {
         setTitle("JanePaint v2");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900, 650);
+        setSize(1050, 650);
         setLocationRelativeTo(null);
         setResizable(false);
         canvas = new DrawingCanva();
@@ -21,6 +21,20 @@ public class PaintF extends JFrame
         JButton clearBtn = new JButton("Clear");
         JButton colorBtn = new JButton("Choose Color");
         JButton eraserBtn = new JButton("Eraser");
+        //Just got add
+        JButton brushBtn = new JButton("Brush");
+        JButton rectBtn = new JButton("Rectangle");
+        JButton ovalBtn = new JButton("Oval");
+        JButton lineBtn = new JButton("Line");
+        //plus
+        JButton saveBtn = new JButton("Save");
+
+        brushBtn.addActionListener(e -> canvas.setTool(DrawingCanva.Tool.BRUSH));
+        rectBtn.addActionListener(e -> canvas.setTool(DrawingCanva.Tool.RECT));
+        ovalBtn.addActionListener(e -> canvas.setTool(DrawingCanva.Tool.OVAL));
+        lineBtn.addActionListener(e -> canvas.setTool(DrawingCanva.Tool.LINE));
+
+        saveBtn.addActionListener(e -> SavingThis.saveImage(canvas.getImage(), this));
 
         JSlider brushSizeSlider = new JSlider(1, 50, 5);
         brushSizeSlider.setPaintTicks(true);
@@ -37,6 +51,7 @@ public class PaintF extends JFrame
             {
                 currentColor = selected;
                 canvas.setColor(currentColor);
+                canvas.setErasing(false); //this from Drawing Canva
             }
         });
         eraserBtn.addActionListener(e -> canvas.setColor(Color.WHITE)
@@ -48,9 +63,17 @@ public class PaintF extends JFrame
         topPanel.add(colorBtn);
         topPanel.add(new JLabel("Brush Size:"));
         topPanel.add(brushSizeSlider);
+        //plus
+        topPanel.add(brushBtn);
+        topPanel.add(rectBtn);
+        topPanel.add(ovalBtn);
+        topPanel.add(lineBtn);
+
+        topPanel.add(saveBtn);
 
         add(topPanel, BorderLayout.NORTH);
         add(canvas, BorderLayout.CENTER);
         setVisible(true);
     }
 }
+//20 lines just got added

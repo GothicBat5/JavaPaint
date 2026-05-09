c7aaa2f5320a058e8cd4823f46ec7c6453a8b165
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-09 19:41:47 +0800

    Update Candy.rs

--- miscellaneous/Candy.rs
+++ miscellaneous/Candy.rs
@@ -1,27 +1,63 @@
 use std::io::{self, Write};
 
-fn main() 
-{
-    print!("Enter a number: ");
-    io::stdout().flush().unwrap(); 
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
-
-    match input.trim().parse::<i32>() {
-        Ok(num) => {
-            if num % 2 == 0 {
-                println!("{} is an even number.", num);
-            } else {
-                println!("{} is an odd number.", num);
-            }
-        }
-        Err(_) => println!("Invalid number"),
+fn is_prime(n: i128) -> bool {
+    if n <= 1 {
+        return false;
     }
+
+    for i in 2..=((n as f64).sqrt() as i128) 
+    {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
 }
 
+fn main() {
+    println!("PRIME, ODD, EVEN");
+
+    print!("Enter a number: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    match input.trim().parse::<f64>() {
+
+        Ok(num) => {
 
 
+            if num.fract() != 0.0 {
+                println!("{} is a DECIMAL number.", num);
+            }
+
+            else {
+                let int_num = num as i128;
+
+                if is_prime(int_num) {
+                    println!("{} is a PRIME number.", int_num);
+                }
+
+                else if int_num % 2 == 0 {
+                    println!("{} is an EVEN number.", int_num);
+                }
+
+                else {
+                    println!("{} is an ODD number.", int_num);
+                }
+            }
+        }
+
+        Err(_) => {
+            println!("Invalid number.");
+        }
+    }
+}
 
 
 

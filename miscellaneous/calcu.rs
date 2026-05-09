a55f24eff774485ad96d97ed563b7608a2da0d61
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-09 19:47:27 +0800

    Update calcu.rs

--- miscellaneous/calcu.rs
+++ miscellaneous/calcu.rs
@@ -1,50 +1,81 @@
-use std::io; 
+use std::io;
 
-fn main()
-{
-    println!("Choose operation: +, -, *, /");
-    let operation = read_input("> ");
-    let num1 = read_number("NumOne: ");
-    let num2 = read_number("NumTwo: ");
-    match operation.trim()
-    {
-        "+" => println!("Result: {}", num1 + num2),
-        "-" => println!("Result: {}", num1 - num2),
-        "*" => println!("Result: {}", num1 * num2),
-        "/" => 
+fn main() {
+
+        println!("Type x to exit.");
+        
+    loop {
+        println!("\nChoose operation: +, -, *, /, %, ^");
+        
+        let operation = read_input("> ");
+
+        if operation.trim().eq_ignore_ascii_case("x") 
         {
-            if num2 == 0.0
-            {
-                println!("Error: Cannot divide by zero");
+            println!("Program Ended.");
+            break;
+        }
+
+        let num1 = read_number("NumOne: ");
+        let num2 = read_number("NumTwo: ");
+
+        match operation.trim() {
+            "+" => println!("Result: {}", num1 + num2),
+
+            "-" => println!("Result: {}", num1 - num2),
+
+            "*" => println!("Result: {}", num1 * num2),
+
+            "/" => {
+                if num2 == 0.0 {
+                    println!("Error: Cannot divide by zero.");
+                } 
+                else {
+                    println!("Result: {}", num1 / num2);
+                }
             }
-            else
-            {
-                println!("Result: {}", num1 / num2);
+
+            "%" => {
+                if num2 == 0.0 {
+                    println!("Error: Cannot modulo by zero.");
+                } 
+                else {
+                    println!("Result: {}", num1 % num2);
+                }
             }
-        },
-        _ => println!("Invalid operation"),
+
+            "^" => {
+                println!("Result: {}", num1.powf(num2));
+            }
+
+            _ => println!("Invalid operation."),
+        }
     }
-    println!("Program Ended.");
 }
-fn read_number(prompt: &str) -> f64
-{
-    loop
-    {
+
+fn read_number(prompt: &str) -> f64 {
+    loop {
         let input = read_input(prompt);
-        match input.trim().parse::<f64>()
-        {
+
+        match input.trim().parse::<f64>() {
             Ok(num) => return num,
             Err(_) => println!("Invalid number."),
         }
     }
 }
-fn read_input(prompt: &str) -> String
-{
+
+fn read_input(prompt: &str) -> String {
     use std::io::Write;
+
     let mut input = String::new();
+
     print!("{}", prompt);
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).expect("Failed to Read.");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read.");
+
     input
 }
+
 

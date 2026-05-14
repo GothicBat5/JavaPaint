97fc76dbad701c909c24bcfb7058e99f649fe8c2
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-15 00:14:52 +0800

    Create Main.java

---
+++ editor /Main.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+      /*
+      The main execution!!
+      */
+        SwingUtilities.invokeLater(Frame::new);
+    }
+}

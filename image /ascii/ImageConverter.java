a598559e9dcfefa5f5efc512a0e03736a59c5078
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-24 03:17:37 +0800

    Update ImageConverter.java

--- image /ascii/ImageConverter.java
+++ image /ascii/ImageConverter.java
@@ -1,59 +1,57 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class ImageConverter {
-
+public class ImageConverter 
+{
     private static final String ASCII_CHARS = "@#S%?*+;:,. ";
-  
-    public static BufferedImage loadImage(File file) {
 
+    public static BufferedImage loadImage(File file) 
+    {
         try {
             return ImageIO.read(file);
-        } catch (IOException e) {
+        } 
+        catch (IOException e) 
+        {
             e.printStackTrace();
             return null;
         }
     }
 
-    public static String convertToASCII(BufferedImage image) {
-
+    public static String convertToASCII(BufferedImage image) 
+    {
         if (image == null) return "Error: Image not loaded";
 
         int width = 120;
-        int height = (image.getHeight() * width) / image.getWidth();
+        double aspectCorrection = 0.47;
+        int height = (int)((image.getHeight() * width * aspectCorrection) / image.getWidth());
+        if (height < 1) height = 1;
 
+        Image scaled = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        resized.getGraphics().drawImage(image, 0, 0, width, height, null);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.drawImage(scaled, 0, 0, null);
+        g2d.dispose();
 
         StringBuilder ascii = new StringBuilder();
-
-        for (int y = 0; y < height; y++) {
-
-            for (int x = 0; x < width; x++) {
-
+        
+        for (int y = 0; y < height; y++) 
+        {
+            for (int x = 0; x < width; x++) 
+            {
                 int rgb = resized.getRGB(x, y);
-
                 int r = (rgb >> 16) & 0xff;
                 int g = (rgb >> 8) & 0xff;
                 int b = rgb & 0xff;
-
-                int brightness = (int)(
-                        0.299 * r +
-                        0.587 * g +
-                        0.114 * b
-                );
-
+                int brightness = (int)(0.299 * r + 0.587 * g + 0.114 * b);
                 int index = (brightness * (ASCII_CHARS.length() - 1)) / 255;
-
                 ascii.append(ASCII_CHARS.charAt(index));
             }
-
             ascii.append("\n");
         }
-
         return ascii.toString();
     }
 }

711a76f1a47dc9c4a00b2f1257ca82c63e8774ca
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-24 03:18:10 +0800

    Update FileLoader.java

--- image /ascii/FileLoader.java
+++ image /ascii/FileLoader.java
@@ -1,27 +1,16 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 
-public class FileLoader {
-
-    public static File chooseImageFile() {
-
+public class FileLoader 
+{
+    public static File chooseImageFile(java.awt.Component parent) 
+    {
         JFileChooser chooser = new JFileChooser();
-
         chooser.setDialogTitle("Select an Image");
-
-        chooser.setFileFilter(
-                new javax.swing.filechooser.FileNameExtensionFilter(
-                        "Image Files (jpg, png, jpeg, bmp)",
-                        "jpg", "jpeg", "png", "bmp"
-                )
-        );
-
-        int result = chooser.showOpenDialog(null);
-
-        if (result == JFileChooser.APPROVE_OPTION) {
-            return chooser.getSelectedFile();
-        }
-
-        return null;
+        chooser.setFileFilter(new FileNameExtensionFilter("Image Files (jpg, png, jpeg, bmp)", "jpg", "jpeg", "png", "bmp"));
+        
+        int result = chooser.showOpenDialog(parent);
+        return (result == JFileChooser.APPROVE_OPTION) ? chooser.getSelectedFile() : null;
     }
 }

81d73b9f4f1d7fe4377a0e983e926d746eb125e0
Gothic_Bat <nrejera.ccsjdm@gmail.com>
2026-08-15 22:22:21 +0800

    Update Main.java

--- src/paint/Main.java
+++ src/paint/Main.java
@@ -6,11 +6,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-                /*
-        * The main execution block as we all know
-        * Place where the main execution code locks in
-        * */
-        
         SwingUtilities.invokeLater(Launcher::new);
     }
 }
